95fb4917a06561f545ac6dc2407b4c3e09073223
mr-jaf01 <sheikjafar32@gmail.com>
2022-04-26 21:26:59 +0100

    Side bar Toggle Button updated

--- resources/views/subject/panel.blade.php
+++ resources/views/subject/panel.blade.php
@@ -39,6 +39,27 @@
    <div id="respone"></div>
 
 </div>
+
+
+
+
+
+
+
+
+
+
+
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/popper.min.js')}}"></script>
+<script src="{{asset('js/moment.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/simplebar.min.js')}}"></script>
+<script src='{{asset('js/daterangepicker.js')}}'></script>
+<script src='{{asset('js/jquery.stickOnScroll.js')}}'></script>
+<script src="{{asset('js/tinycolor-min.js')}}"></script>
+<script src="{{asset('js/config.js')}}"></script>
+<script src="{{asset('js/apps.js')}}"></script>
 @endsection
 <style>
     @keyframes growProgressBar {
